fe0e43c394c2e8a4b719415257bb26782b89b353
amalisashini <amalisashini@gmail.com>
2023-07-19 20:14:04 +0530

    Initial Commit

---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,67 @@
+package tests;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import dev.failsafe.internal.util.Assert;
+import pageobjects.LoginPage;
+import pageobjects.Profile;
+import resources.Base;
+
+public class LoginTest extends Base {
+
+   WebDriver driver;
+	
+	@Test
+    (dataProvider="getLoginData")
+	public void login(String email,String password,String expectedResult) throws IOException, InterruptedException {
+
+        driver = initializeBrowser();
+		driver.get(prop.getProperty("consumerurl"));
+	
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.emailAddressTextField().sendKeys(email);
+		loginPage.passwordField().sendKeys(password);
+		loginPage.loginButton().click();
+		
+		Profile profilePage = new Profile(driver);
+		
+		String actualResult = null;
+		
+		try {
+			
+			if(profilePage.profileName().isDisplayed()) {
+			   actualResult = "Success";
+			}
+			
+		}catch(Exception e) {
+			
+			actualResult = "Failure";
+			
+		}
+		
+		Assert.assertEquals(actualResult,expectedResult);
+		
+	}
+	
+	@AfterMethod
+	public void closure() {
+		
+		driver.close();
+		
+	}
+	
+	@DataProvider
+	public Object[][] getLoginData() {
+		
+		Object[][] data = {{"oversight_user","welCome1/","Success"},{"dummy@test.com","1234","Failure"}};
+		
+		return data;
+		
+	}
+
+}
